c3319160c30f69294105c914b5e7c47627b973ae
Yurii M <ispoyar@yandex.ru>
2025-08-16 16:24:13 +0300

    transferring icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Basket, Header, Navigation, Search, Slider } from "@/components";
+import { Header, Navigation, Slider } from "@/components";
 
 export default function Home() {
   return (
     <div className="font-sans m-auto grid grid-rows-[981px_auto_113px] items-start justify-items-center w-(--w-layout)">
-      <Header search={<Search />} basket={<Basket />} slider={<Slider />}>
+      <Header slider={<Slider />}>
         <Navigation />
       </Header>
       <main className="text-(length:--text-size-xxl)">main</main>
