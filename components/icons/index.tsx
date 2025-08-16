0841b51a6b5d56ee4b1d368afcceab09193ba3b1
Yurii M <ispoyar@yandex.ru>
2025-08-16 16:10:35 +0300

    added icons

---
+++ components/icons/index.tsx
@@ -0,0 +1,74 @@
+export const iconArrowLeft = (
+  <svg
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M22.6958 14.4355H5.48568M5.48568 14.4355C5.48568 14.4355 10.364 11.2272 11.3552 8.56592M5.48568 14.4355C5.48568 14.4355 9.79826 17.45 11.3552 20.305"
+      stroke="currentColor"
+      strokeWidth="2.17391"
+      strokeLinecap="square"
+    />
+  </svg>
+);
+export const iconArrowRight = (
+  <svg
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5.3042 14.4355H22.5143M22.5143 14.4355C22.5143 14.4355 17.636 11.2272 16.6448 8.56592M22.5143 14.4355C22.5143 14.4355 18.2017 17.45 16.6448 20.305"
+      stroke="currentColor"
+      strokeWidth="2.17391"
+      strokeLinecap="square"
+    />
+  </svg>
+);
+export const iconPlus = (
+  <svg
+    width="25"
+    height="25"
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M22.1343 12.2812C22.1343 11.5121 21.5108 10.8885 20.7416 10.8885H13.7784V3.92525C13.7784 3.15611 13.1548 2.53259 12.3857 2.53259C11.6165 2.53259 10.993 3.15611 10.993 3.92525V10.8885H4.02974C3.2606 10.8885 2.63708 11.5121 2.63708 12.2812C2.63708 13.0503 3.2606 13.6739 4.02974 13.6739H10.993V20.6371C10.993 21.4063 11.6165 22.0298 12.3857 22.0298C13.1548 22.0298 13.7784 21.4063 13.7784 20.6371V13.6739H20.7416C21.5108 13.6739 22.1343 13.0503 22.1343 12.2812Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+export const iconBracketLeft = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M13.293 17.707L7.58603 12L13.293 6.29297L14.707 7.70697L10.414 12L14.707 16.293L13.293 17.707Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+export const iconBracketRight = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10.707 17.707L16.414 12L10.707 6.29297L9.29297 7.70697L13.586 12L9.29297 16.293L10.707 17.707Z"
+      fill="currentColor"
+    />
+  </svg>
+);
