0f221d5a40fe2000e14d6e83f9c9988aca4ae3e9
Yurii M <ispoyar@yandex.ru>
2025-08-25 14:48:34 +0300

    added customer-reviews mock

---
+++ mocks/customer-reviews.ts
@@ -0,0 +1,49 @@
+import { TCustomerReview } from "@/types";
+
+export const customerReviews: TCustomerReview[] = [
+  {
+    id: "1",
+    avatar: "/customers/user-1.jpg",
+    name: "Robert Adams",
+    profession: "Coach",
+    message:
+      "Sint est culpa elit ullamco proident adipisicing irure excepteur. Proident tempor culpa irure et est incididunt elit quis consequat. Exercitation et reprehenderit amet ad laborum minim enim ea mollit.",
+    productImage: "/image-10.jpg",
+  },
+  {
+    id: "2",
+    avatar: "/customers/user-2.jpg",
+    name: "Mark Graham",
+    profession: "Сook",
+    message:
+      "Reprehenderit elit magna excepteur deserunt officia deserunt. Eu non eiusmod consequat laboris tempor laborum mollit ad reprehenderit magna ad. Exercitation culpa duis occaecat commodo ad id dolore do Lorem cupidatat pariatur et eiusmod ad. Occaecat cupidatat ex veniam ex ullamco sint eu do dolore adipisicing ut consequat exercitation. Eiusmod ad quis id occaecat culpa.",
+    productImage: "/image-11.jpg",
+  },
+  {
+    id: "3",
+    avatar: "/customers/user-3.jpg",
+    name: "Jessica Howell",
+    profession: "Builder",
+    message:
+      "Cillum irure tempor aute laborum consequat sint. Culpa occaecat sit ad magna enim proident esse voluptate. Ullamco fugiat non nostrud ipsum commodo. Do sit in ex ea fugiat. Eu labore anim sunt ex cillum dolor commodo velit cupidatat consequat id culpa mollit. Laborum elit ut est do laborum veniam proident. Irure sint deserunt amet reprehenderit quis quis esse do adipisicing.",
+    productImage: "/image-12.jpg",
+  },
+  {
+    id: "4",
+    avatar: "/customers/user-4.jpg",
+    name: "Samantha",
+    profession: "Teacher",
+    message:
+      "Lorem fugiat consequat ea elit velit. Minim mollit aliqua pariatur veniam eiusmod ullamco occaecat. Consectetur ipsum commodo amet deserunt pariatur quis labore commodo commodo et ullamco nisi adipisicing. Laborum in commodo nostrud est duis in excepteur est qui ipsum. Aliquip quis culpa dolor eiusmod excepteur cupidatat exercitation nulla consequat velit esse.",
+    productImage: "/image-13.jpg",
+  },
+  {
+    id: "5",
+    avatar: "/customers/user-5.jpg",
+    name: "Clementine Bauch",
+    profession: "Housewife",
+    message:
+      "Deserunt adipisicing do quis deserunt ea elit. Velit ullamco in fugiat tempor dolor dolore labore eiusmod velit cillum nostrud ullamco. Commodo nostrud tempor cillum Lorem consectetur voluptate. Cillum quis magna officia ut eiusmod do. Culpa mollit fugiat aliquip eu ea esse voluptate sit ullamco non.",
+    productImage: "/image-14.jpg",
+  },
+];
