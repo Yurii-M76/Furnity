9244dbc62b27034dcbfe12672b67d40f1864a7f3
Yurii M <ispoyar@yandex.ru>
2025-08-25 14:49:34 +0300

    update CustomerReviews

--- components/customer-reviews/customer-reviews.tsx
+++ components/customer-reviews/customer-reviews.tsx
@@ -1,58 +1,76 @@
+"use client";
 import Image from "next/image";
-import { FC, ReactNode } from "react";
+import { useState } from "react";
 import { CircleButton } from "../buttons";
+import { customerReviews } from "@/mocks/customer-reviews";
+import { TCustomerReview } from "@/types";
 
-type TCustomerReviews = {
-  children?: ReactNode;
-};
+const CustomerReviews = () => {
+  const [currentNumReview, setCurrentNumReview] = useState(0);
+  const itemReview: TCustomerReview = customerReviews[currentNumReview];
+  const reviewsLength = customerReviews.length;
 
-const CustomerReviews: FC<TCustomerReviews> = () => {
   return (
     <section className="relative flex flex-row items-center justify-center text-white bg-[#19191B] w-full pt-[70px] pb-[70px]">
       <div className="absolute left-48 rotate-[-30deg] w-[172px] h-[897px] bg-white opacity-4" />
       <section className="flex flex-row items-center justify-between w-(--w-section)">
         <div className="flex flex-col w-[517px] gap-[50px]">
           <h2 style={{ color: "currentcolor" }}>What are they saying ?</h2>
-          <div className="flex flex-col gap-[20px]">
-            <div className="flex flex-row gap-[15px]">
-              <div className="flex flex-row w-[50px] h-[50px] shrink-0 rounded-[50%] overflow-hidden">
-                <Image
-                  src="/avatar-1.png"
-                  alt="avatar"
-                  width={50}
-                  height={50}
-                  style={{ objectFit: "cover" }}
-                />
+          <div className="flex flex-col gap-[30px]">
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-row gap-[15px]">
+                <div className="flex flex-row w-[50px] h-[50px] shrink-0 rounded-[50%] overflow-hidden">
+                  <Image
+                    src={itemReview.avatar}
+                    alt="user-photo"
+                    width={50}
+                    height={50}
+                    style={{ objectFit: "cover" }}
+                    loading="lazy"
+                  />
+                </div>
+                <div className="flex flex-col gap-[5px]">
+                  <span className="font-bold">{itemReview.name}</span>
+                  <span className="text-(length:--text-size-sm) opacity-70">
+                    {itemReview.profession}
+                  </span>
+                </div>
               </div>
-              <div className="flex flex-col gap-[5px]">
-                <span className="font-bold">Jane Lowis</span>
-                <span className="text-(length:--text-size-sm) opacity-70">
-                  Housewife
-                </span>
-              </div>
+              <p className="max-h-[190px] overflow-auto">
+                {itemReview.message}
+              </p>
             </div>
-            <p className="max-h-[190px] overflow-auto">
-              the services and products offered are very good, matched what I
-              was looking for. now my room is more beautiful and aesthetic.
-            </p>
             <div className="flex flex-row gap-[20px] mt-[10px]">
               <CircleButton
                 variant="bracket-left"
                 color="watermelon"
-                disabled
+                onClick={() =>
+                  currentNumReview > 0 &&
+                  setCurrentNumReview(currentNumReview - 1)
+                }
+                disabled={!currentNumReview}
               />
-              <CircleButton variant="bracket-right" color="watermelon" />
+              <CircleButton
+                variant="bracket-right"
+                color="watermelon"
+                onClick={() =>
+                  currentNumReview !== reviewsLength - 1 &&
+                  setCurrentNumReview(currentNumReview + 1)
+                }
+                disabled={currentNumReview === reviewsLength - 1}
+              />
             </div>
           </div>
         </div>
         <div className="flex flex-row w-[488px] h-[462px] shrink-0 rounded-[15px] overflow-hidden">
           <Image
-            src="/image-10.jpg"
+            src={itemReview.productImage}
             alt="image"
             width={600}
             height={600}
             style={{ objectFit: "cover" }}
             className="hover:scale-110 transition ease-in-out duration-1250"
+            loading="lazy"
           />
         </div>
       </section>
