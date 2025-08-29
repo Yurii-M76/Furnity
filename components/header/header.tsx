85da88a33cac67b7d68a3e9dd38df32073e5e89f
Yurii M <ispoyar@yandex.ru>
2025-08-29 13:58:18 +0300

    added modal to header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,6 +1,8 @@
-import { FC, ReactNode } from "react";
+"use client";
+import { FC, ReactNode, useState } from "react";
 import { Button } from "../buttons";
 import { iconBasket, iconLogo, iconSearch } from "../icons";
+import { Modal } from "../modal";
 
 type THeader = {
   slider?: ReactNode;
@@ -8,6 +10,8 @@ type THeader = {
 };
 
 const Header: FC<THeader> = ({ children, slider }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <header className="grid grid-cols-1 grid-rows-[192px_274px_480px] items-start justify-items-center w-full h-full bg-top bg-no-repeat bg-[url(/cover.png)] font-family">
       <section className="flex flex-row flex-none items-top justify-between mt-[24px] w-(--w-section) h-full gap-[8px]">
@@ -27,13 +31,33 @@ const Header: FC<THeader> = ({ children, slider }) => {
           we have helped more than 1000+ people to give perfect comfort to the
           interior of their home
         </p>
-          <Button label="Find Produk" color="accent" size="lg" />
+        <Button
+          label="Find Produk"
+          color="accent"
+          size="lg"
+          onClick={() => setIsModalOpen(!isModalOpen)}
+        />
       </section>
       {slider && (
         <section className="w-[1240px] pl-[20px] pr-[20px] mt-[30px]">
           {slider}
         </section>
       )}
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(!isModalOpen)}
+        title="Find Produk"
+      >
+        <p>
+          ollit pariatur enim consectetur non ut consectetur culpa quis ut magna
+          consectetur. Aute ipsum proident et elit id occaecat deserunt elit
+          incididunt aliqua. Voluptate nulla aute ipsum sit proident eu non
+          ipsum aliquip ut minim esse. Laborum deserunt dolore duis ut magna
+          reprehenderit dolore ea veniam non dolor irure elit ad. Tempor
+          occaecat consequat aliquip cupidatat minim do deserunt.
+        </p>
+        <Button label="close" size="md" onClick={() => setIsModalOpen(!isModalOpen)} />
+      </Modal>
     </header>
   );
 };
